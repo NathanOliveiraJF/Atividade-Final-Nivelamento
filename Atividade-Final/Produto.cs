07187899849a4dab7509d7c454962adcc24d34d8
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2020-12-26 11:24:11 -0300

    add Produto

---
+++ Atividade-Final/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_Final
+{
+    public class Produto
+    {
+
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public double Preco { get; set; }
+        public double Custo { get; set; }
+        public double Lucro()
+        {
+           return Preco - Custo;
+        }
+    }
+}
