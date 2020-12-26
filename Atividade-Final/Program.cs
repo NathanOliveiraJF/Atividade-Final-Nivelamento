45e9737195f1817637ce36820e63100e89962a01
NathanOliveiraJF <nathanoliveira003jf@gmail.com>
2020-12-26 11:28:12 -0300

    add menu de opcoes

--- Atividade-Final/Program.cs
+++ Atividade-Final/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Atividade_Final
 {
@@ -6,7 +7,36 @@ namespace Atividade_Final
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var produtos = new Produto[100];
+            var sair = 1;
+            string escolha;
+
+
+            while (sair != 0)
+            {
+                Opcoes();
+                escolha = Console.ReadLine();
+
+                Console.Write("\nsair digite 0 / 1 para continuar: ");
+                sair = Convert.ToInt32(Console.ReadLine());
+            }
+
         }
+
+        static void Opcoes()
+        {
+            Console.WriteLine("\n#########################");
+            Console.WriteLine("[ A ] Cadastrar Produto");
+            Console.WriteLine("[ B ] Atualizar o preço de um produto");
+            Console.WriteLine("[ C ] Imprimir o preço médio dos produtos");
+            Console.WriteLine("[ D ] Imprimir listagem de produtos");
+            Console.WriteLine("#########################");
+
+            Console.Write("Selecione uma letra: ");
+
+        }
+
+       
+
     }
 }
